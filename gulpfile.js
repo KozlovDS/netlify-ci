28da79bbad79d2df2db479965b16e3ad4d962b54
Dmitry Kozlov <kozlovv.ds@yandex.ru>
2023-11-26 23:44:31 +0300

    подготовили к публикации1~

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,33 @@ const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+}
+
 function html() {
   return gulp.src('src/**/*.html')
-  .pipe(plumber())
-  .pipe(gulp.dest('dist/'))
-  .pipe(browserSync.reload({stream: true}));
+        .pipe(plumber())
+				.pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}));
 }
 
 function css() {
   return gulp.src('src/blocks/**/*.css')
         .pipe(plumber())
         .pipe(concat('bundle.css'))
-                .pipe(gulp.dest('dist/'))
-                .pipe(browserSync.reload({stream: true}));
+				.pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}));
 }
 
 function images() {
   return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-            .pipe(gulp.dest('dist/images'))
-            .pipe(browserSync.reload({stream: true}));
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({stream: true}));
 }
 
 function clean() {
@@ -35,17 +43,14 @@ function watchFiles() {
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
 }
 
-function serve() {
-  browserSync.init({
-    server: {
-      baseDir: './dist'
-    }
-  });
-}
-
 const build = gulp.series(clean, gulp.parallel(html, css, images));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
+exports.html = html;
+exports.css = css;
+exports.images = images;
+exports.clean = clean;
+
 exports.build = build;
-exports.watchapp = watchapp; 
-exports.default = watchapp; 
+exports.watchapp = watchapp;
+exports.default = watchapp;
